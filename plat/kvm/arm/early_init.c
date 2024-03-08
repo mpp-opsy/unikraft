30b2b134f5f52a09bf0429c0fbfd5898ef8cb27c
Michalis Pappas <michalis@unikraft.io>
2024-03-08 06:22:12 +0100

    plat/kvm/arm64: Add early init boot stage
    Add boot stage for early initialization. Currently this is implemented
    as a simple placeholder from where drivers can directly call their
    early_init(). Eventually it shall be replaced with a proper callback
    registration framework provided by ukboot.
    
    The early init boot stage is invoked as soon as the MMU is enabled and
    before bootinfo is populated with information provided by the
    firmware, as bootinfo_setup depends on early UART for debug messages.
    Early devices can use the available APIs to obtain any information
    required from the firmware, such as device regions and the kernel
    command line.
    
    Drivers at early init must append mrds to bootinfo for any regions
    they wish to survive paged memory initialization and remain mapped
    until driver init(). These regions must use the UKPLAT_MEMRT_DEVICE
    type. Coalescing bootinfo mrds is performed one time at at the end
    of bootinfo_setup().
    
    Signed-off-by: Michalis Pappas <michalis@unikraft.io>

---
+++ plat/kvm/arm/early_init.c
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/* Copyright (c) 2023, Unikraft GmbH and The Unikraft Authors.
+ * Licensed under the BSD-3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ */
+
+#include <uk/assert.h>
+#include <uk/essentials.h>
+
+void kvm_early_init_fdt(void *fdt)
+{
+	/* Placeholder for early device initialization */
+}
